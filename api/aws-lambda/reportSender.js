87c6c8fc444c89f2c324e819e28aad8b931303ad
Pradeepsingh Bhati <bh.pradeepsingh@gmail.com>
2021-04-10 00:16:35 +0530

    [Add] lambda function for sending emails from dynamoDb triggers

---
+++ api/aws-lambda/reportSender.js
@@ -0,0 +1,26 @@
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: "us-east-1" });
+
+
+exports.handler = async function (event) {
+    const source = "bh.pradeepsingh@gmail.com";
+    event.Records.forEach(async (record) => {
+        const destination = record.dynamodb.NewImage.emailId.S;
+        const link = record.dynamodb.NewImage.docLink.S;
+        var params = {
+            Destination: {
+                ToAddresses: [destination],
+            },
+            Message: {
+                Body: {
+                    Text: { Data: "Your Lecturizer generated lecture report link : " + link },
+                },
+
+                Subject: { Data: "Lecturizer Report" },
+            },
+            Source: source,
+        }
+        console.log(params);
+        await ses.sendEmail(params).promise();
+    });
+};
